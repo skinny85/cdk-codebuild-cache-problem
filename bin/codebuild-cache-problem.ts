8115f871b38785bd53185dc1d80e1c0a35829f7d
Adam Ruka <adamruka@amazon.com>
2020-11-23 11:01:23 -0800

    Initial commit

---
+++ bin/codebuild-cache-problem.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { CodebuildCacheProblemStack } from '../lib/codebuild-cache-problem-stack';
+
+const app = new cdk.App();
+new CodebuildCacheProblemStack(app, 'CodebuildCacheProblemStack');
