49c170ce09134945ad332fb05ad4f2e07659ec3e
Adam Ruka <adamruka@amazon.com>
2020-11-23 11:30:50 -0800

    Add the CodeBuild cache code.

--- bin/codebuild-cache-problem.ts
+++ bin/codebuild-cache-problem.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CodebuildCacheProblemStack } from '../lib/codebuild-cache-problem-stack';
--- lib/codebuild-cache-problem-stack.ts
+++ lib/codebuild-cache-problem-stack.ts
@@ -1,9 +1,17 @@
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as s3 from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 
 export class CodebuildCacheProblemStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new codebuild.Project(this, 'Project', {
+      source: codebuild.Source.bitBucket({
+        owner: 'awslabs',
+        repo: 'aws-cdk',
+      }),
+      cache: codebuild.Cache.bucket(new s3.Bucket(this, 'Bucket'))
+    });
   }
 }
